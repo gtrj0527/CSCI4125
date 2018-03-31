b60591033286ae6b9e4d8c8c86b06318693b7dcb
Rachel Guillory <rachel.n.guillory@gmail.com>
2018-03-31 12:32:14 -0500

    Query 14, first cut.  Division...  I don't like it.

---
+++ Query_14.sql
@@ -0,0 +1,20 @@
+-- Query #14
+-- has_skills / ks_code
+-- has_cert / cert_code
+CREATE VIEW skill_qual AS SELECT DISTINCT pers_id FROM has_skills AS hs1
+WHERE NOT EXISTS
+      (SELECT * FROM position_skills AS ps1
+       WHERE pos_code = ? AND NOT EXISTS
+          (SELECT * FROM has_skills AS hs2
+           WHERE hs1.pers_id = hs2.pers_id AND
+                 hs2.ks_code = ps1.ks_code));
+CREATE VIEW cert_qual AS SELECT DISTINCT pers_id FROM has_cert AS hc1
+WHERE NOT EXISTS
+      (SELECT * FROM position_cert AS pc1
+       WHERE pos_code = ? AND NOT EXISTS
+          (SELECT * FROM has_cert AS hc2
+           WHERE hc1.pers_id = hc2.pers_id AND
+                 hc2.cert_code = pc1.cert_code));
+                 
+SELECT last_name, first_name, mi, email FROM person NATURAL JOIN skill_qual 
+                                                    NATURAL JOIN cert_qual;
