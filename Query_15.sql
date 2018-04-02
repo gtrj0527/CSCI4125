ce6fa18986eb97cc15b52cc660fd303442f2d752
Rachel Guillory <rachel.n.guillory@gmail.com>
2018-04-02 16:15:38 -0500

    Fixed using AS, shows my MySQL skills...  At work, no time to create certs, assuming that he is expecting certs at this level.  Considering we (I) are supposed to use certs, probably so.  But, with the appropriately populated tables, it should work.  Take off the NATURAL JOIN with the cert_qual, and you'll get the appropriate person.

--- Query_15.sql
+++ Query_15.sql
@@ -1,20 +1,25 @@
 -- Query #15
 -- has_skills / ks_code
 -- has_cert / cert_code
-CREATE VIEW skill_qual AS SELECT DISTINCT pers_id FROM has_skills AS hs1
+
+INSERT INTO has_skill VALUES (1, 'Java2');
+
+DROP VIEW skill_qual;
+CREATE VIEW skill_qual AS SELECT DISTINCT pers_id FROM has_skill hs1
 WHERE NOT EXISTS
-      (SELECT * FROM position_skills AS ps1
-       WHERE pos_code = ? AND NOT EXISTS
-          (SELECT * FROM has_skills AS hs2
+      (SELECT * FROM position_skills ps1
+       WHERE pos_code = 10 AND NOT EXISTS
+          (SELECT * FROM has_skill hs2
            WHERE hs1.pers_id = hs2.pers_id AND
                  hs2.ks_code = ps1.ks_code));
-CREATE VIEW cert_qual AS SELECT DISTINCT pers_id FROM has_cert AS hc1
+DROP VIEW cert_qual;
+CREATE VIEW cert_qual AS SELECT DISTINCT pers_id FROM has_cert hc1
 WHERE NOT EXISTS
-      (SELECT * FROM position_cert AS pc1
-       WHERE pos_code = ? AND NOT EXISTS
-          (SELECT * FROM has_cert AS hc2
+      (SELECT * FROM position_cert pc1
+       WHERE pos_code = 10 AND NOT EXISTS
+          (SELECT * FROM has_cert hc2
            WHERE hc1.pers_id = hc2.pers_id AND
                  hc2.cert_code = pc1.cert_code));
                  
-SELECT last_name, first_name, mi, email FROM person NATURAL JOIN skill_qual 
-                                                    NATURAL JOIN cert_qual;
+SELECT last_name, first_name, mi, email FROM person NATURAL JOIN skill_qual; 
+                                                    --NATURAL JOIN cert_qual;
