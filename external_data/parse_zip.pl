6a8b26a69ddfe1f95892c65e5556a92571418c1e
Rachel Guillory <rachel.n.guillory@gmail.com>
2018-04-10 23:04:52 -0500

    Added external zip->city, state database.

---
+++ external_data/parse_zip.pl
@@ -0,0 +1,27 @@
+my $zipcode_file = "free-zipcode-database-Primary.csv";
+my $out_file = "zip_code_insert.sql";
+
+open ZIP, "<$zipcode_file";
+open OUT, ">$out_file";
+
+my $header = <ZIP>;
+$header =~ s/\"//g;
+
+my @headers = split ",", $header;
+
+my %zip_hash;
+my $base_zip_sql = "INSERT INTO zip_code (zip_code, city, state) (ZIP, CITY, STATE)";
+while(<ZIP>) {
+
+	my ($zip_code, $a, $city, $state, $b) = split ",";
+	my $zip_sql = $base_zip_sql;
+	$zip_code =~ s/\"/\'/g;
+	$city =~ s/\"/\'/g;
+	$state =~ s/\"/\'/g;
+	$zip_sql =~ s/ZIP/$zip_code/;
+	$zip_sql =~ s/CITY/$city/;
+	$zip_sql =~ s/STATE/$state/;
+
+	print OUT $zip_sql . ";\n";
+
+}
