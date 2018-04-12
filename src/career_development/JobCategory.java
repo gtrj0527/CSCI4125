7de6e30ecea2458380582440425b26a14467fad5
Jeri Marchan <jtmarch1@uno.edu>
2018-04-12 18:28:24 -0500

    Job Category class and jobCategories runner --untested -jtm

---
+++ src/career_development/JobCategory.java
@@ -0,0 +1,185 @@
+package career_development;
+
+import java.sql.*;
+import java.util.LinkedList;
+import oracle.jdbc.OracleTypes;
+import oracle.jdbc.internal.OraclePreparedStatement;
+
+public class JobCategory {
+
+    private String catCode;
+    private String parentCatCode;
+    private String coreSkillCode;
+    private String jobCategoryTitle;
+    private String jobCategoryDescription;
+    private Float payRangeHigh;
+    private Float payRangeLow;
+    private boolean dirty; //potentially inconsistent with DB
+
+    public static LinkedList<JobCategory> retrieveAllJobCategories(Connection conn) {
+        PreparedStatement retrJobCategory;
+        LinkedList<JobCategory> jobCategoryList = new LinkedList<>();
+        try {
+            retrJobCategory = conn.prepareStatement("SELECT catCode, parentCatCode, coreSkillCode, jobCategoryTitle, " +
+                    "jobCategoryDescription,payRangeHigh, payRangeLow FROM job_category");
+            ResultSet rs = retrJobCategory.executeQuery();
+            while (rs.next()) {
+                String catCode = rs.getString(1);
+                String parentCatCode = rs.getString(2);
+                String coreSkillCode = rs.getString(3);
+                String jobCategoryTitle = rs.getString(4);
+                String jobCategoryDescription = rs.getString(5);
+                Float payRangeHigh = rs.getFloat(6);
+                Float payRangeLow = rs.getFloat(7);
+                jobCategoryList.add(new JobCategory(catCode, parentCatCode, coreSkillCode, jobCategoryTitle, jobCategoryDescription, payRangeHigh, payRangeLow));
+            }
+
+        } catch (SQLException sqlEx) {
+            System.err.println(sqlEx.toString());
+            return null;
+        }
+        return jobCategoryList;
+    }
+
+//    public static retrieveJobCategory (String catCode, Connection conn) {
+//        PreparedStatement retrJobCategory;
+//        try {
+//            retrJobCategory = conn.prepareStatement("SELECT catCode, parentCatCode, coreSkillCode, jobCategoryTitle," +
+//                    "jobCategoryDescription,payRangeHigh, payRangeLow FROM job_category WHERE cat_code = ?");
+//            retrJobCategory.setString(1,catCode);
+//            ResultSet rs = retrJobCategory.executeQuery();
+//            rs.next();
+//            String parentCatCode = rs.getString(1);
+//            String coreSkillCode = rs.getString(2);
+//            String jobCategoryTitle = rs.getString(3);
+//            String jobCategoryDescription =rs.getString(4);
+//            Float payRangeHigh = rs.getFloat(5);
+//            Float payRangeLow = rs.getFloat(6);
+//            return new JobCategory(catCode, parentCatCode, coreSkillCode, jobCategoryTitle, jobCategoryDescription, payRangeHigh, payRangeLow);
+//        } catch (SQLException sqlEx) {
+//            System.err.println(sqlEx.toString());
+//        }
+//    }
+
+
+    public JobCategory() {
+        this.catCode = catCode;
+        this.parentCatCode = parentCatCode;
+        this.coreSkillCode = coreSkillCode;
+        this.jobCategoryTitle = jobCategoryTitle;
+        this.jobCategoryDescription = jobCategoryDescription;
+        this.payRangeHigh = payRangeHigh;
+        this.payRangeLow = payRangeLow;
+    }
+
+    private JobCategory(String catCode, String parentCatCode, String coreSkillCode, String jobCategoryTitle,
+                       String jobCategoryDescription, Float payRangeHigh, Float payRangeLow) {
+       // this(catCode, parentCatCode, coreSkillCode, jobCategoryTitle, jobCategoryDescription, payRangeHigh, payRangeLow);
+    }
+
+
+    public String getCatCode() {
+        return catCode;
+    }
+
+    public void setCatCode(String catCode) {
+        this.dirty = this.catCode.equals(catCode);
+        this.catCode = catCode;
+    }
+
+    public String getParentCatCode() {
+        return parentCatCode;
+    }
+
+    public void setParentCatCode(String parentCatCode) {
+        this.dirty = this.parentCatCode.equals(parentCatCode);
+        this.parentCatCode = parentCatCode;
+    }
+
+    public String getCoreSkillCode() {
+        return coreSkillCode;
+    }
+
+    public void setCoreSkillCode(String coreSkillCode) {
+        this.dirty = this.coreSkillCode.equals(coreSkillCode);
+        this.coreSkillCode = coreSkillCode;
+    }
+
+    public String getJobCategoryTitle() {
+        return jobCategoryTitle;
+    }
+
+    public void setJobCategoryTitle(String jobCategoryTitle) {
+        this.dirty = this.jobCategoryTitle.equals(jobCategoryTitle);
+        this.jobCategoryTitle = jobCategoryTitle;
+    }
+
+    public String getJobCategoryDescription() {
+        return jobCategoryDescription;
+    }
+
+    public void setJobCategoryDescription(String jobCategoryDescription) {
+        this.dirty = this.jobCategoryDescription.equals(jobCategoryDescription);
+        this.jobCategoryDescription = jobCategoryDescription;
+    }
+
+    public Float getPayRangeHigh() {
+        return payRangeHigh;
+    }
+
+    public void setPayRangeHigh(Float payRangeHigh) {
+        this.dirty = this.payRangeHigh.equals(payRangeHigh);
+        this.payRangeHigh = payRangeHigh;
+    }
+
+    public Float getPayRangeLow() {
+        return payRangeLow;
+    }
+
+    public void setPayRangeLow(Float payRangeLow) {
+        this.dirty = this.payRangeLow.equals(payRangeLow);
+        this.payRangeLow = payRangeLow;
+    }
+
+
+//    public void commit (Connection conn) {
+//        if(!this.dirty) {
+//            return;
+//        }
+//        if(catCode==null) {
+//            this.store(conn);
+//        } else {
+//            JobCategory dbJobCategory = JobCategory.retrieveAllJobCategories(conn);
+//        }
+//    }
+
+
+    private void store (Connection conn) {
+        try {
+            OraclePreparedStatement preparedStatement =
+                    (OraclePreparedStatement)conn.prepareStatement("INSERT INTO job_category (cat_code, parent_cat_code, core_skill, job_category_title, " +
+                            "job_description, pay_range_high, pay_range_low) VALUES (?,?,?,?,?,?,?)");
+            preparedStatement.registerReturnParameter(7, OracleTypes.INTEGER);
+            preparedStatement.setString(1, catCode);
+            preparedStatement.setString(2, parentCatCode);
+            preparedStatement.setString(3, coreSkillCode);
+            preparedStatement.setString(4, jobCategoryTitle);
+            preparedStatement.setString(5, jobCategoryDescription);
+            preparedStatement.setFloat(6, payRangeHigh);
+            preparedStatement.setFloat(7, payRangeLow);
+            ResultSet lastIDrs = preparedStatement.getReturnResultSet();
+            if(lastIDrs.next()) {
+                String catCode = lastIDrs.getString(1);
+                System.out.println(catCode);
+                this.catCode=catCode;
+            }
+
+
+
+
+        } catch (SQLException sqlEx) {
+            System.err.println(sqlEx.toString());
+        }
+        this.dirty=false;
+    }
+}
--- src/career_development/Main.java
+++ src/career_development/Main.java
@@ -1,40 +1,40 @@
-package career_development;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.Iterator;
-import java.util.List;
-
-public class Main {
-
-    //TODO: Add close for statements and connection
-    public static void main(String[] args) {
-        try {
-            String userName = "";
-            String password = "";
-            String hostName = "dbsvcs.cs.uno.edu";
-            int port = 1521;
-            String sid = "orcl";
-            Connection conn = DriverManager.getConnection("jdbc:oracle:thin:" + userName + "/" + password + "@" + hostName + ":" + port + ":" + sid);
-            Course course = Course.retrieveCourse(1, conn);
-            System.out.println("Title: " + course.getTitle());
-            List<Course> list = Course.retrieveAllCourses(conn);
-            Iterator<Course> courseIterator = list.iterator();
-            while(courseIterator.hasNext()) {
-                Course c = courseIterator.next();
-                System.out.println("c_code: " + c.getCCode() + " Title: " + c.getTitle());
-            }
-            Course c = new Course ("Sample Course2", "Intermediate", "E", 1234.54,
-                    "Traditional", "Check to see if this is working.");
-            c.commit(conn);
-            Integer newCCode = c.getCCode();
-            Course testCourse = Course.retrieveCourse(newCCode, conn);
-            System.out.println(testCourse.getRetailPrice());
-        } catch (SQLException sqlEx) {
-            System.err.println(sqlEx.toString());
-            System.err.println("Connection failed");
-        }
-	// write your code here
-    }
-}
+//package career_development;
+//
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.SQLException;
+//import java.util.Iterator;
+//import java.util.List;
+//
+//public class Main {
+//
+//    //TODO: Add close for statements and connection
+//    public static void main(String[] args) {
+//        try {
+//            String userName = "jtmarch1";
+//            String password = "Tg4zJWx7";
+//            String hostName = "dbsvcs.cs.uno.edu";
+//            int port = 1521;
+//            String sid = "orcl";
+//            Connection conn = DriverManager.getConnection("jdbc:oracle:thin:" + userName + "/" + password + "@" + hostName + ":" + port + ":" + sid);
+//            Course course = Course.retrieveCourse(1, conn);
+//            System.out.println("Title: " + course.getTitle());
+//            List<Course> list = Course.retrieveAllCourses(conn);
+//            Iterator<Course> courseIterator = list.iterator();
+//            while(courseIterator.hasNext()) {
+//                Course c = courseIterator.next();
+//                System.out.println("c_code: " + c.getCCode() + " Title: " + c.getTitle());
+//            }
+//            Course c = new Course ("Sample Course2", "Intermediate", "E", 1234.54,
+//                    "Traditional", "Check to see if this is working.");
+//            c.commit(conn);
+//            Integer newCCode = c.getCCode();
+//            Course testCourse = Course.retrieveCourse(newCCode, conn);
+//            System.out.println(testCourse.getRetailPrice());
+//        } catch (SQLException sqlEx) {
+//            System.err.println(sqlEx.toString());
+//            System.err.println("Connection failed");
+//        }
+//	// write your code here
+//    }
+//}
