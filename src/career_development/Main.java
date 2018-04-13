dd01e027d97f41eb8a14618ba6bc727016dd0b7e
Gaby Swanson <gtrj@hotmail.com>
2018-04-13 18:00:20 -0500

    Classes set up for Position, Section, and Skills

--- src/career_development/Main.java
+++ src/career_development/Main.java
@@ -11,12 +11,14 @@ public class Main {
     //TODO: Add close for statements and connection
     public static void main(String[] args) {
         try {
-            String userName = "";
-            String password = "";
+            String userName = "gtswanso";
+            String password = "sNNP9R9R";
             String hostName = "dbsvcs.cs.uno.edu";
             int port = 1521;
             String sid = "orcl";
             Connection conn = DriverManager.getConnection("jdbc:oracle:thin:" + userName + "/" + password + "@" + hostName + ":" + port + ":" + sid);
+
+            /**/
             Course course = Course.retrieveCourse(1, conn);
             System.out.println("Title: " + course.getTitle());
             List<Course> list = Course.retrieveAllCourses(conn);
@@ -25,12 +27,31 @@ public class Main {
                 Course c = courseIterator.next();
                 System.out.println("c_code: " + c.getCCode() + " Title: " + c.getTitle());
             }
-            Course c = new Course ("Sample Course2", "Intermediate", "E", 1234.54,
-                    "Traditional", "Check to see if this is working.");
-            c.commit(conn);
-            Integer newCCode = c.getCCode();
-            Course testCourse = Course.retrieveCourse(newCCode, conn);
-            System.out.println(testCourse.getRetailPrice());
+//
+//            /*Test Course */
+//            Course c = new Course ("Sample Course2", "Intermediate", "E", 1234.54,
+//                    "Traditional", "Check to see if this is working.");
+//            c.commit(conn);
+//            Integer newCCode = c.getCCode();
+//            Course testCourse = Course.retrieveCourse(newCCode, conn);
+//            System.out.println(testCourse.getRetailPrice());
+
+            /*Test Person */
+            Person person = Person.retrievePerson(1, conn);
+            System.out.println("Last Name: " + person.getLastName());
+            List<Person> listPerson = Person.retrieveAllPeople(conn);
+            Iterator<Person> personIterator = listPerson.iterator();
+            while(personIterator.hasNext()) {
+                Person p = personIterator.next();
+                System.out.println("pers_id: " + p.getPersID() + " Last Name: " + p.getLastName());
+            }
+            Person p = new Person ("Organa", "Leia", "S",
+                    "87 Republic Way", "Suite 27A", "84392", "leia.organa@republic.org",
+                    "F");
+            p.commit(conn);
+            Integer newPersID = p.getPersID();
+            Person testPerson = Person.retrievePerson(newPersID, conn);
+            System.out.println(testPerson.getEmail());
         } catch (SQLException sqlEx) {
             System.err.println(sqlEx.toString());
             System.err.println("Connection failed");
---
+++ src/career_development/Section.java
@@ -0,0 +1,10 @@
+package career_development;
+
+import java.sql.*;
+import java.util.LinkedList;
+import oracle.jdbc.OracleTypes;
+import oracle.jdbc.internal.OraclePreparedStatement;
+
+public class Section {
+
+}
