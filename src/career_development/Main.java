8a488c18c61ade053ae82253979dea4d2fe94290
Gaby Swanson <gtrj@hotmail.com>
2018-04-19 22:04:15 -0500

    Removed password

--- src/career_development/Main.java
+++ src/career_development/Main.java
@@ -19,8 +19,8 @@ public class Main {
     //TODO: Add close for statements and connection
     public static void main(String[] args) {
         try {
-            String userName = "jtmarch1";
-            String password = "Tg4zJWx7";
+            String userName = "gtswanson";
+            String password = "sNNP9R9R";
 
             String hostName = "dbsvcs.cs.uno.edu";
             int port = 1521;
