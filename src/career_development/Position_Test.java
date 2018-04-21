fd7001989c3a797a2c09da5529705823ab5531c2
Rachel Guillory <rachel.n.guillory@gmail.com>
2018-04-21 16:36:38 -0500

    Almost qualified implemented, in Position_Test.  Fixed type of comp_id in Position to Integer, and added a retrieve by comp_id.

--- src/career_development/Position_Test.java
+++ src/career_development/Position_Test.java
@@ -12,49 +12,59 @@ import java.util.List;
 /**
  * Created by Gaby's Laptop on 4/19/2018.
  */
-public class Position_Test {public static void main(String[] args) {
-    try {
-        String userName = "";
-        String password = "";
+public class Position_Test {
+        public static void main(String[] args) {
+            try {
+                String userName = "";
+                String password = "";
 
-        String hostName = "dbsvcs.cs.uno.edu";
-        int port = 1521;
-        String sid = "orcl";
-        Connection conn = DriverManager.getConnection("jdbc:oracle:thin:" + userName + "/" + password + "@" + hostName + ":" + port + ":" + sid);
+                String hostName = "dbsvcs.cs.uno.edu";
+                int port = 1521;
+                String sid = "orcl";
+                Connection conn = DriverManager.getConnection("jdbc:oracle:thin:" + userName + "/" + password + "@" + hostName + ":" + port + ":" + sid);
 
-        Position position = Position.retrievePosition(1, conn);
-        List<Position> listPosition = Position.retrieveAllPositions(conn);
-        Iterator<Position> positionIterator = listPosition.iterator();
-        while (positionIterator.hasNext()) {
-            Position pos = positionIterator.next();
-            System.out.println("pos_code: " + pos.getPosCode() + " Position Title: " + pos.getPosTitle());
+                Position position = Position.retrievePosition(1, conn);
+                List<Position> listPosition = Position.retrieveAllPositions(conn);
+                Iterator<Position> positionIterator = listPosition.iterator();
+                while (positionIterator.hasNext()) {
+                    Position pos = positionIterator.next();
+                    System.out.println("pos_code: " + pos.getPosCode() + " Position Title: " + pos.getPosTitle());
+                }
+                Position pos = new Position(1116, "DB Administrator", "FT",
+                        "15-1250", "98000", "S");
+                pos.commit(conn);
+                Integer newPosCode = pos.getPosCode();
+                Position testPosition = Position.retrievePosition(newPosCode, conn);
+                System.out.println(testPosition.getPosTitle());
+                Position p = Position.retrievePosition(7, conn);
+                LinkedList<Person> qualPeople = p.getQualifiedPeople(conn);
+                Iterator<Person> qualIterator = qualPeople.iterator();
+                while(qualIterator.hasNext()) {
+                    Person pers = qualIterator.next();
+                    System.out.println(pers.getPersID());
+                }
+                Person chris = Person.retrievePerson(7, conn);
+                Skill ang3 = Skill.retrieveSkill("Angular3", conn);
+                boolean beforeAdd = p.isQualified(chris, conn);
+                System.out.println(beforeAdd);
+                chris.addSkill(ang3, conn);
+                boolean afterAdd = p.isQualified(chris, conn);
+                System.out.println(afterAdd);
+                Person person = Person.retrievePerson(6, conn);
+                boolean qualified = p.isQualified(person, conn);
+                System.out.println(qualified);
+                Position posTwelve = Position.retrievePosition(12, conn);
+                LinkedList<Person> almostQualified = new LinkedList<Person>();
+                Integer minMissingSkills = posTwelve.findAlmostQualifiedPeople(conn, almostQualified);
+                System.out.println(minMissingSkills);
+                Iterator<Person> almostIterator = almostQualified.iterator();
+                while(almostIterator.hasNext()) {
+                    Person person1 = almostIterator.next();
+                    System.out.println(person1.getLastName());
+                }
+            }
+        catch (SQLException sqlEx) {
+            System.err.println(sqlEx.toString());
         }
-        Position pos = new Position("1116", "DB Administrator", "FT",
-                "15-1250", "98000", "S");
-        pos.commit(conn);
-        Integer newPosCode = pos.getPosCode();
-        Position testPosition = Position.retrievePosition(newPosCode, conn);
-        System.out.println(testPosition.getPosTitle());
-        Position p = Position.retrievePosition(7, conn);
-        LinkedList<Person> qualPeople = p.getQualifiedPeople(conn);
-        Iterator<Person> qualIterator = qualPeople.iterator();
-        while(qualIterator.hasNext()) {
-            Person pers = qualIterator.next();
-            System.out.println(pers.getPersID());
-        }
-        Person chris = Person.retrievePerson(7, conn);
-        Skill ang3 = Skill.retrieveSkill("Angular3", conn);
-        boolean beforeAdd = p.isQualified(chris, conn);
-        System.out.println(beforeAdd);
-        chris.addSkill(ang3, conn);
-        boolean afterAdd = p.isQualified(chris, conn);
-        System.out.println(afterAdd);
-        Person person = Person.retrievePerson(6, conn);
-        boolean qualified = p.isQualified(person, conn);
-        System.out.println(qualified);
     }
-    catch (SQLException sqlEx) {
-        System.err.println(sqlEx.toString());
-    }
-}
 }
